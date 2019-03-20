4ea80e649a877c6b9cecd12740318db550515759
dandelionadia <nadia.ridko@gmail.com>
2019-03-20 14:59:30 +0100

    adds component and styles of Card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './card.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const arr = [
+    {
+        id: 1,
+        name: 'Adventure Time',
+        type: 'adventure'
+    },
+    {
+        id: 2,
+        name: 'Avatar. The Legend of Aang',
+        type: 'history'
+    }
+]
+
+class Cards extends React.Component {
+    render() {
+        const card = this.props.data.map(function(item) {
+            return (
+                <div className="card" key={item.id}>
+                    <p className="card__name">{item.name}</p>
+                    <p className="card__Type">{item.type}</p>
+                </div>
+            )
+        })
+        return (
+            <div>{card}</div>
+        )
+    }
+}
+
+const App = () => {
+    return (
+        <div>
+            <Cards data={arr} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
