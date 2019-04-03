b1940e3bb3b1f492dbfc11042725efc98879618f
dandelionadia <nadia.ridko@gmail.com>
2019-04-03 14:05:51 +0200

    adds start made about cartoons

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './card.scss';
+import './aboutCartoons.css';
 import * as serviceWorker from './serviceWorker';
 
 import cartoons from './cartoons.json'
+import aboutCartoons from './aboutCartoons.json'
 
 class CardType extends React.Component {
     render() {
@@ -23,6 +25,28 @@ class CardType extends React.Component {
     }
 }
 
+class AboutCartoons extends React.Component {
+    render() {
+        const AboutCartoons = this.props.data.map(function (item) {
+            return (
+                <div class="grid-container">
+                    <div className="image">{item.image}</div>
+                    <div className="content">
+                        <h3>{item.content.title}</h3>
+                        <div>{item.content.description}</div>
+                    </div>
+                    <div className="apisodes">{item.apisodes}
+                        <div><a href={item.apisodes.href}>{item.apisodes.nameApisodes}</a></div>
+                    </div>
+                </div>
+            )
+        })
+        return (
+            <div>{AboutCartoons}</div>
+        )
+    }
+}
+
 const Image = ({ src }) => {
     return <img className="cardType__img" src={src} alt="img" />
 }
@@ -31,10 +55,13 @@ const App = () => {
     return (
         <div>
             <CardType data={cartoons} />
+            <AboutCartoons data={aboutCartoons} />
         </div>
     )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'));
 
 serviceWorker.unregister();
