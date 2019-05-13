dda3030e7829d6acf97daa59fcfefc99f89fd231
dandelionadia <nadia.ridko@gmail.com>
2019-05-13 16:32:30 +0200

    adds start cards characters

--- src/Characters.js
+++ src/Characters.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './characters.scss';
 
 class Characters extends React.Component {
     render() {
-
         return (
             <div>
                 <h3>Characters</h3>
                 <div className="characters">
                     {this.props.data.characters.map((character) => (
-                        <div>
-                            <img src={character.img} alt="image" />
-                            <p>{character.name}</p>
+                        <div className="character">
+                            <img className="characters__img" src={character.img} alt="image" />
+                            <p><span className="characters__name">Name:</span>{character.name}</p>
+                            <p><span className="characters__name">What does he likes:</span>{character.color}</p>
                         </div>
                     ))}
                 </div>
